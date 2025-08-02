cd46715c5c0c7b7524e9b6a6fcd95292d993062d
dharmendra016 <dharmendra.078bct016@tcioe.edu.np>
2025-08-02 17:16:57 +0545

    map-fixing

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"
-import { AuthProvider } from "@/contexts/AuthContext";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,10 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-        <Toaster />
+        {children}
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,8 @@
-import MapComponent from "@/components/MapComponent";
-import Link from "next/link";
-
+import Map from "@/components";
 export default function Home() {
-  
   return (
-      
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-center mt-10">Welcome to the Delivery App</h1>
-      <p className="text-center mt-4">This is a simple delivery app built with Next.js and Socket.IO.</p>
-      <div className="flex justify-center mt-10">
-        <Link href="/login" className="px-6 py-3 bg-blue-500 text-white rounded-2xl hover:bg-blue-600">Login</Link>
-        <Link href="/register" className="ml-4 px-6 py-3 bg-green-500 text-white rounded-2xl hover:bg-green-600">Register</Link>
-      </div>
-      <div className="mt-10 text-center">
-        <p className="text-gray-600">© 2023 Delivery App. All rights reserved.</p>
-      </div>
-      <MapComponent/>
+    <div>
+      <Map />
     </div>
-
   );
 }
